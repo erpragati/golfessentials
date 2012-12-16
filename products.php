b0656e8ed43c2c070a04a65fcdb0a1f3f669e379
narayanwaraich <narayanwaraich@gmail.com>
2012-12-16 18:12:45 +0530

    lot more images added

--- products.php
+++ products.php
@@ -500,11 +500,11 @@ elseif (isset($_GET['s'])) {
 ?>
 
 <?php include 'footer.php'; ?>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js.gz"></script>
 <script>window.jQuery || document.write('<script src="js/libs/jquery-1.8.2.min.js"><\/script>')</script>
 <script src="js/plugins.js"></script>
-<script src="js/script.js"></script>
-<script type="text/javascript" src="js/jquery.nivo.slider.js"></script>
+<script src="js/script.js.gz"></script>
+<script type="text/javascript" src="js/jquery.nivo.slider.js.gz"></script>
 <script type="text/javascript">
 $(window).load(function() {
     $('#slider').nivoSlider();
